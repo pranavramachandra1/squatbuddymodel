c611498e9513493cdba867a599e5695ebbfb2e57
pranavramachandra1 <pranavramachandra@berkeley.edu>
2025-02-09 12:09:50 -0600

    unnec packages

--- app/main.py
+++ app/main.py
@@ -1,8 +1,3 @@
 from app import create_app
-from dotenv import load_dotenv
-import os
-
-# load environment variables:
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), "..", ".env"))
 
 app = create_app()
